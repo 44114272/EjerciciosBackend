0691f55183cf2fb50d3c8e0fa0a42ebae0007259
Joaquín Elía <joaquin.elia@hotmail.com>
2023-06-14 22:14:46 -0300

    Class 22 passport 2

---
+++ class22/exercise/src/routes/auth.router.js
@@ -0,0 +1,62 @@
+import {Router} from 'express';
+import passport from 'passport';
+import { authtorization, generateToken, passportCall } from '../utils.js';
+
+const router = Router();
+
+const users = [{
+    name: 'Joaquin',
+    email: 'joaquin.elia@hotmail.com',
+    password: '1234'
+}];
+
+router.post('/register', async (req, res) => {
+    try { 
+        const {name, email, password} = req.body;
+        const exists = users.find(user => user.email === email);
+        if(exists) return res.status(400).send({status: 'error', error: 'User already exists'});
+
+        const user = {
+            name,
+            email,
+            password
+        };
+
+        users.push(user);
+
+        const accessToken = generateToken(user);
+
+        res.send({status: 'success', access_token: accessToken});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({status: 'error', error: error});
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = users.find(user => user.email === email && user.password === password);
+        user.role = 'user';
+
+        if (!user) return res.status(400).send({ status: 'error', error: 'Invalid credentials' });
+
+        const accessToken = generateToken(user);
+        
+        res.cookie('coderCookieToken', accessToken, {
+            maxAge: 60 * 60 * 1000, httpOnly: true}
+        ).send({status: 'success'});
+    } catch (error) {
+        res.status(500).send({status: 'error', error: error});
+    }
+});
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) =>{
+    res.send({status: 'success', payload: req.user});
+});
+
+router.get('/current-custom', passportCall('jwt'), authtorization('admin'), (req, res) =>{
+    res.send({status: 'success', payload: req.user});
+});
+
+export default router;
